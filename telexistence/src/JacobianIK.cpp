3a634ff900a5468b99898f5cbca02b9b1f2e9f7f
apparettedare <ksuzuki9541@gmail.com>
2023-08-14 17:03:15 +0900

    aaa

--- telexistence/src/JacobianIK.cpp
+++ telexistence/src/JacobianIK.cpp
@@ -1,41 +1,65 @@
 #include "sobit_mini_library/sobit_mini_controller.hpp"
 #include "oculus_telexistence/Part.h"
 #include <ros/ros.h>
+#include <sensor_msgs/JointState.h>
 #include <cmath>
 #include <iostream>
 
 using namespace std;
 
 // ジョイントの角度
-double theta1, theta2, theta3;
+double shoulder_roll, shoulder_pan, elbow_tilt, wrist_tilt, body_roll;
 
 // 目標位置
 double targetX = 0.0;
 double targetY = 0.0;
+double targetZ = 0.0;
+double rotationX = 0.0;
+double rotationY = 0.0;
+double rotationZ = 0.0;
 
 // リンクの長さ
-const double arm_shoulder = 110.96352;
 const double arm_elbow = 102.57457;
 const double arm_wrist = 156.55295;
+const double arm_shoulder = 108.75985;
+const double shoulder_width = 195.0;
 
 // ヤコビ行列を計算する関数
-void calculateJacobian(double theta1, double theta2, double theta3, double jacobian[2][3]) {
+void calculateJacobian_2d(double shoulder_roll, double elbow_tilt, double wrist_tilt, double jacobian[2][3]) {
     // ヤコビ行列の要素を計算
-    jacobian[0][0] = -arm_shoulder * sin(theta1) - arm_elbow * sin(theta1 + theta2) - arm_wrist * sin(theta1 + theta2 + theta3);
-    jacobian[0][1] = -arm_elbow * sin(theta1 + theta2) - arm_wrist * sin(theta1 + theta2 + theta3);
-    jacobian[0][2] = -arm_wrist * sin(theta1 + theta2 + theta3);
+    jacobian[0][0] = arm_shoulder * cos(shoulder_roll) + arm_elbow * cos(shoulder_roll + elbow_tilt) + arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt);
+    jacobian[0][1] = arm_elbow * cos(shoulder_roll + elbow_tilt) + arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt);
+    jacobian[0][2] = arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt);
     
-    jacobian[1][0] = arm_shoulder * cos(theta1) + arm_elbow * cos(theta1 + theta2) + arm_wrist * cos(theta1 + theta2 + theta3);
-    jacobian[1][1] = arm_elbow * cos(theta1 + theta2) + arm_wrist * cos(theta1 + theta2 + theta3);
-    jacobian[1][2] = arm_wrist * cos(theta1 + theta2 + theta3);
+    jacobian[1][0] = -arm_shoulder * sin(shoulder_roll) - arm_elbow * sin(shoulder_roll + elbow_tilt) - arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt);
+    jacobian[1][1] = -arm_elbow * sin(shoulder_roll + elbow_tilt) - arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt);
+    jacobian[1][2] = -arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt);
 }
 
-void InverseKinematics() {
+// void calculateJacobian_3d(double shoulder_roll, double elbow_tilt, double wrist_tilt, double jacobian3d[3][4]) {
+//     // ヤコビ行列の要素を計算
+//     jacobian3d[0][0] = ;
+//     jacobian3d[0][1] = ;
+//     jacobian3d[0][2] = ;
+//     jacobian3d[0][3] = ;
+
+//     jacobian3d[1][0] = ;
+//     jacobian3d[1][1] = ;
+//     jacobian3d[1][2] = ;
+//     jacobian3d[1][3] = ;
+
+//     jacobian3d[2][0] = ;
+//     jacobian3d[2][1] = ;
+//     jacobian3d[2][2] = ;
+//     jacobian3d[2][3] = ;
+
+// }
+void InverseKinematics_2d() {
     // 逆運動学の解を格納する変数
-    double deltaTheta1, deltaTheta2, deltaTheta3;
+    double deltashoulder_roll, deltaelbow_tilt, deltawrist_tilt;
     
     // 収束条件
-    double epsilon = 0.01;
+    double epsilon = 0.1;
     
     // 最大反復回数
     int maxIterations = 1000;
@@ -48,8 +72,9 @@ void InverseKinematics() {
     
     // 逆運動学の反復計算
     do {
+        cout << "ヤコビってます" << endl;
         // ヤコビ行列を計算
-        calculateJacobian(theta1, theta2, theta3, jacobian);
+        calculateJacobian_2d(shoulder_roll, elbow_tilt, wrist_tilt, jacobian);
         
         // ヤコビ行列の擬似逆行列を計算
         double jacobianInverse[3][2];
@@ -62,52 +87,147 @@ void InverseKinematics() {
         jacobianInverse[2][1] = -(jacobian[0][0] * jacobian[2][1] - jacobian[0][1] * jacobian[2][0]) / determinant;
         
         // 目標位置と現在の位置の差分を計算
-        double deltaX = targetX - (arm_shoulder * cos(theta1) + arm_elbow * cos(theta1 + theta2) + arm_wrist * cos(theta1 + theta2 + theta3));
-        double deltaY = targetY - (arm_shoulder * sin(theta1) + arm_elbow * sin(theta1 + theta2) + arm_wrist * sin(theta1 + theta2 + theta3));
+        double deltaX = targetX - (arm_shoulder * sin(shoulder_roll) + arm_elbow * sin(shoulder_roll + elbow_tilt) + arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt));
+        double deltaZ = targetZ - (arm_shoulder * sin(shoulder_roll) + arm_elbow * sin(shoulder_roll + elbow_tilt) + arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt));
         
         // 角度の変化量を計算
-        deltaTheta1 = jacobianInverse[0][0] * deltaX + jacobianInverse[0][1] * deltaY;
-        deltaTheta2 = jacobianInverse[1][0] * deltaX + jacobianInverse[1][1] * deltaY;
-        deltaTheta3 = jacobianInverse[2][0] * deltaX + jacobianInverse[2][1] * deltaY;
+        deltashoulder_roll = jacobianInverse[0][0] * deltaX + jacobianInverse[0][1] * deltaZ;
+        deltaelbow_tilt = jacobianInverse[1][0] * deltaX + jacobianInverse[1][1] * deltaZ;
+        deltawrist_tilt = jacobianInverse[2][0] * deltaX + jacobianInverse[2][1] * deltaZ;
         
         // 角度を更新
-        theta1 += deltaTheta1;
-        theta2 += deltaTheta2;
-        theta3 += deltaTheta3;
+        shoulder_roll += deltashoulder_roll;
+        elbow_tilt += deltaelbow_tilt;
+        elbow_tilt += deltawrist_tilt;
         
         // 反復回数をインクリメント
         iteration++;
-    } while ((fabs(deltaTheta1) > epsilon || fabs(deltaTheta2) > epsilon || fabs(deltaTheta3) > epsilon) && iteration < maxIterations);
+    } while ((fabs(deltashoulder_roll) > epsilon || fabs(deltaelbow_tilt) > epsilon || fabs(deltawrist_tilt) > epsilon) && iteration < maxIterations);
     
     // 結果を出力
-    cout << "Theta1: " << theta1 << endl;
-    cout << "Theta2: " << theta2 << endl;
-    cout << "Theta3: " << theta3 << endl;
+    cout << "shoulder_roll: " << shoulder_roll << endl;
+    cout << "elbow_tilt: " << elbow_tilt << endl;
+    cout << "wrist_tilt: " << wrist_tilt << endl;
     
     sobit_mini::SobitMiniController mini_arm_ctr;
-    mini_arm_ctr.moveRightArm( theta1, 0, -theta2, -theta3, 0.0);
+    // smini_arm_ctr.moveRightArm( shoulder_roll, -1.4, elbow_tilt, wrist_tilt, 0.0);
 
 }
 
+// void InverseKinematics_3d() {
+//     // 逆運動学の解を格納する変数
+//     double deltashoulder_roll, deltaelbow_tilt, deltawrist_tilt;
+    
+//     // 収束条件
+//     double epsilon = 0.01;
+    
+//     // 最大反復回数
+//     int maxIterations = 1000;
+    
+//     // 反復回数
+//     int iteration = 0;
+    
+//     // ヤコビ行列
+//     double jacobian3d[3][4];
+    
+//     // 逆運動学の反復計算
+//     do {
+//         // ヤコビ行列を計算
+//         // calculateJacobian_3d(shoulder_roll, shoulder_pan, elbow_tilt, jacobian3d);
+        
+//         // ヤコビ行列の擬似逆行列を計算
+
+        
+//         // 目標位置と現在の位置の差分を計算
+//         double deltaX = targetX - (arm_shoulder * sin(shoulder_roll) + arm_elbow * sin(shoulder_roll + elbow_tilt) + arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt));
+//         double deltaZ = targetZ - (arm_shoulder * sin(shoulder_roll) + arm_elbow * sin(shoulder_roll + elbow_tilt) + arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt));
+        
+//         // 角度の変化量を計算
+//         deltashoulder_roll = jacobianInverse[0][0] * deltaX + jacobianInverse[0][1] * deltaZ;
+//         deltaelbow_tilt = jacobianInverse[1][0] * deltaX + jacobianInverse[1][1] * deltaZ;
+//         deltawrist_tilt = jacobianInverse[2][0] * deltaX + jacobianInverse[2][1] * deltaZ;
+        
+//         // 角度を更新
+//         shoulder_roll += deltashoulder_roll;
+//         elbow_tilt += deltaelbow_tilt;
+//         elbow_tilt += deltawrist_tilt;
+        
+//         // 反復回数をインクリメント
+//         iteration++;
+//     } while ((fabs(deltashoulder_roll) > epsilon || fabs(deltaelbow_tilt) > epsilon || fabs(deltawrist_tilt) > epsilon) && iteration < maxIterations);
+    
+//     // 結果を出力
+//     cout << "shoulder_roll: " << shoulder_roll << endl;
+//     cout << "elbow_tilt: " << elbow_tilt << endl;
+//     cout << "wrist_tilt: " << wrist_tilt << endl;
+    
+//     sobit_mini::SobitMiniController mini_arm_ctr;
+//     mini_arm_ctr.moveRightArm( shoulder_roll, -1.4, elbow_tilt, wrist_tilt, 0.0);
+
+// }
 void Kinematics() {
-    double trigonometric[2];
-    trigonometric[0] = arm_shoulder * cos(theta1) + arm_elbow * cos(theta1 + theta2) + arm_wrist * cos(theta1 + theta2 + theta3);
-    trigonometric[1] = arm_shoulder * sin(theta1) + arm_elbow * sin(theta1 + theta2) + arm_wrist * sin(theta1 + theta2 + theta3);
+    double trigonometric[3];
+    double trigonometric_3d[3];
+    trigonometric[0] = arm_shoulder * sin(shoulder_roll) + arm_elbow * sin(shoulder_roll + elbow_tilt) + arm_wrist * sin(shoulder_roll + elbow_tilt + wrist_tilt);
+    trigonometric[1] = 174;
+    trigonometric[2] = 141.953 + arm_shoulder * cos(shoulder_roll) + arm_elbow * cos(shoulder_roll + elbow_tilt) + arm_wrist * cos(shoulder_roll + elbow_tilt + wrist_tilt);
+    double point0[3] = {0, 174, -141.953};
+    double point1[3] = {point0[0] + arm_shoulder * sin(-shoulder_pan) * sin(shoulder_roll), point0[1] + arm_shoulder * cos(shoulder_pan), point0[2] + arm_shoulder * sin(shoulder_pan) * cos(shoulder_roll)};
+    double t[2] = {arm_elbow * cos(-elbow_tilt) + arm_wrist * cos(-elbow_tilt - wrist_tilt), arm_elbow * sin(-elbow_tilt) + arm_wrist * sin(-elbow_tilt  - wrist_tilt)};
+    double point01_vec[3] = {(point1[0] - point0[0]) / arm_shoulder, (point1[1] - point0[1]) / arm_shoulder , (point1[2] - point0[2]) / arm_shoulder};
+    trigonometric_3d[0] = point1[0] + point01_vec[0] * t[0] + cos(shoulder_roll) * t[1];
+    trigonometric_3d[1] = point1[1] + point01_vec[1] * t[0];
+    trigonometric_3d[2] = point1[2] + point01_vec[2] * t[0] + sin(shoulder_roll) * t[1];
+
     cout << "x座標: " << trigonometric[0] << endl;
-    cout << "y座標: " << trigonometric[1] << endl;
+    // cout << "y座標: " << trigonometric[1] << endl;
+    cout << "z座標: " << -trigonometric[2]<< endl << endl;
 }
 
 void position_sub(const oculus_telexistence::Part::ConstPtr& data) {
-    targetX = data -> Right.position.x;
-    targetY = data -> Right.position.y;
+    targetX = data -> Left.position.x;
+    targetY = data -> Left.position.y;
+    targetZ = data -> Left.position.z;
+    rotationX = data -> Left.rotation.x;
+    rotationY = data -> Left.rotation.y;
+    rotationZ = data -> Left.rotation.z;
 }
 
+void position_cb(const sensor_msgs::JointState::ConstPtr& data) {
+    shoulder_roll = data -> position[3];
+    shoulder_pan = data -> position[4];
+    elbow_tilt = -data -> position[5];
+    wrist_tilt = -data -> position[6];
+    body_roll = data -> position[2];
+}
+
+void attyon(){
+    cout << "x座標: " << targetZ  << endl;
+    cout << "z座標: " << targetY << endl << endl;
+    cout << "修正" << endl << "x座標: " << round(targetZ * 367.887 / 0.6 * 1000) / 1000  << endl;
+    cout << "z座標: " << round(targetY * 509.84 / 0.9 * 1000) / 1000 << endl << endl;
+    // cout << "y回転: " << rotationX << endl;
+    // cout << "z回転: " << rotationY << endl;
+    // cout << "x回転: " << rotationZ << endl << endl;
+}
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "sobit_mini_arm_control_oculus_node");
+  ros::init(argc, argv, "jacobian_inverse_kinematics_node");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/oculus/3d", 10, position_sub);
-  InverseKinematics();
-  Kinematics();
+  ros::Subscriber sub = nh.subscribe("/joint_states", 10, position_cb);
+  // ros::Subscriber sub = nh.subscribe("/oculus/3d", 10, position_sub);
+  ros::Rate loop_rate(1);  // 10Hzのループレート
+  while (ros::ok()){
+    Kinematics();
+    // InverseKinematics_2d();
+    // attyon();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
   ros::spin();
+  return 0;
 }
 
+//367.887
+//-509.84
+
+//-141.953
--- telexistence/src/sub_pantilt.cpp
+++ telexistence/src/sub_pantilt.cpp
@@ -8,47 +8,79 @@ double sclpan = 2.2;
 double scltilt = 1.2;
 double scl_velocity_y = 3.0;
 double scl_velocity_x = 3.0;
+float first_pan = 0.0;
+float first_tilt = 0.0;
 float pan = 0.0;
 float tilt = 0.0;
 float next_pan = 0.0;
 float next_tilt = 0.0;
 float pan_v = 0.0;
 float tilt_v = 0.0;
-void oculussub(const oculus_telexistence::Part::ConstPtr& data) {
+bool flag;
+void oculuscb(const oculus_telexistence::Part::ConstPtr& data) {
 
     pan = round(data -> Head.rotation.y * sclpan * 10)/10;
     tilt = round(data -> Head.rotation.x * scltilt * 10)/10;
-    // pan_v = round(data -> Head.velocity.x * scl_velocity_x * 10)/10;
-    // tilt_v = round(data -> Head.velocity.y * scl_velocity_y * 10)/10;
-
-    if(pan >= 1.5) pan = 1.5;
-    else if(pan <= -1.5) pan = -1.5;
-    if(tilt >= 0.5) tilt = 0.5;
-    else if(tilt <= -0.5) tilt = -0.5;
+    flag = true;
     // if(pan_v < 0) pan_v *= -1;
     // if(tilt_v < 0) tilt_v *= -1;
 
     // ROS_INFO("Subscribe rotation.x: %f", tilt);
     // ROS_INFO("Subscribe rotation.y: %f", pan);
-    ROS_INFO("Subscribe velocity.x: %f", pan_v);
-    ROS_INFO("Subscribe velocity.y: %f", tilt_v);
+    // pan_v = round(data -> Head.velocity.x * scl_velocity_x * 10)/10;
+    // tilt_v = round(data -> Head.velocity.y * scl_velocity_y * 10)/10;
 
-    // ros::Duration(0.5).sleep();
-    sobit_mini::SobitMiniController mini_pantilt_ctr;
-    if(pan != next_pan || tilt != next_tilt){
-        printf("moving\n");
-        mini_pantilt_ctr.moveHeadPanTilt(-pan, -tilt, 0.5, false);
-        ros::Duration(0.01).sleep();
-    }
-    next_pan = pan;
-    next_tilt = tilt;
 
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "sobit_mini_pantilt_control_oculus_node");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/oculus/3d", 10, oculussub);
+  ros::Subscriber sub = nh.subscribe("/oculus/3d", 10, oculuscb);
+  flag = false;
+  while (ros::ok())        
+  {
+      ros::spinOnce();     
+      if (flag)            
+      {
+          break;           
+      }
+      ros::spinOnce();     
+  }
+  first_pan = pan;
+  first_tilt = tilt;
+  ros::spinOnce();
+
+  while (ros::ok()){
+    // printf("first_pan: %f\n", first_pan);
+    // printf("first_tilt: %f\n", first_tilt);
+    // printf("pan: %f\n", pan);
+    // printf("tilt: %f\n", tilt);
+    // pan -= first_pan;
+    // tilt -= first_tilt;
+    ros::spinOnce();
+    if(pan >= 1.5) pan = 1.5;
+    else if(pan <= -1.5) pan = -1.5;
+    if(tilt >= 0.5) tilt = 0.5;
+    else if(tilt <= -0.5) tilt = -0.5;
+    ros::spinOnce();
+
+    // ROS_INFO("Subscribe velocity.x: %f", pan_v);
+    // ROS_INFO("Subscribe velocity.y: %f", tilt_v);
+
+    // ros::Duration(1).sleep();
+    sobit_mini::SobitMiniController mini_pantilt_ctr;
+    if(pan != next_pan || tilt != next_tilt){
+        printf("moving\n");
+        ros::spinOnce();
+        mini_pantilt_ctr.moveHeadPanTilt(-pan, -tilt, 0.5, false);
+        ros::Duration(0.01).sleep();
+    }
+    ros::spinOnce();
+    next_pan = pan;
+    next_tilt = tilt;
+  }
   ros::spin();
+  return 0;
 }
 
